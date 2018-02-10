fe18a2b957965447735e3addd5b67778eb4ba22f
RustumZ <36330543+RustumZ@users.noreply.github.com>
2018-02-10 09:30:46 -0500

    Add Bouncy Ball Project

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	Paddle paddle;
+	private Vector3 paddleToBallVector;
+	public bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		// if the game has not started
+		if (!hasStarted) {
+			// Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			// if the mouse is clicked
+			if (Input.GetMouseButtonDown(0)) {
+				print("Mouse Clicked, launching ball");
+				
+				// the game starts
+				hasStarted = true;
+				
+				// launches the ball
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+			}
+		}
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		Vector2 tweak = new Vector2 (Random.Range(-0.2f, 0.2f), Random.Range(-0.2f, 0.2f));
+		
+		if (hasStarted) {
+			audio.Play();
+			rigidbody2D.velocity += tweak;
+		}
+	}
+}
---
+++ Block Breaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+	
+	public AudioClip crack;
+	public Sprite[] hitSprites;
+	public static int breakableCount = -1;
+	public static bool queenDestroyed;
+	
+	private int timesHit = 0;
+	private LevelManager levelManager;
+	private bool isBreakable;
+	private Ball ball;
+
+	// Use this for initialization
+	void Start () {
+		ball = GameObject.FindObjectOfType<Ball>();
+		queenDestroyed = false;
+		
+		isBreakable = (this.tag == "Breakable") || (this.tag == "Queen");
+		levelManager = GameObject.FindObjectOfType<LevelManager> ();
+		
+		if (isBreakable && ball.hasStarted == false) {
+			breakableCount++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		AudioSource.PlayClipAtPoint (crack, transform.position, 0.25f);
+		if (isBreakable) {
+			HandleHits();
+		}
+	}
+	
+	
+	void HandleHits () {
+		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+		
+		if (timesHit >= maxHits) {
+			if (this.tag == "Queen") {
+				queenDestroyed = true;
+			}
+			print(breakableCount);
+			breakableCount--;
+			levelManager.BrickDestroyed();
+			Destroy(gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+	
+	
+	void LoadSprites () {
+		int spriteIndex = timesHit - 1;
+		
+		if (hitSprites[spriteIndex]) {
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		}
+	}
+	
+	// TODO Remove this method once we actually win!
+	
+	void SimulateWin () {
+		levelManager.LoadNextLevel();
+	}
+}
