28906a40b5acaab3cdc1840cc7f2e296583c84d2
RustumZ <36330543+RustumZ@users.noreply.github.com>
2018-02-18 08:54:52 -0500

    Edited Bouncy Ball Project

--- Bouncy Ball/Assets/_Scripts/DifficultyIncreaser.cs
+++ Bouncy Ball/Assets/_Scripts/DifficultyIncreaser.cs
@@ -3,42 +3,64 @@ using System.Collections;
 
 public class DifficultyIncreaser : MonoBehaviour {
 
+	public Color[] BgColor;
+	public Camera cam;
+	public Block block;
+
 	public float blockSepDecrease;
 	public float ballSpeedIncrease;
 	public float BlockSpeedIncrease;
-
+	public float BlockSep = 6f;
 	public float increaseAmount;
-	public Color[] BgColor;
-	public Camera cam;
 
-	float whenLevelIncreases;
-	BlockCloner blockCloner;
-	int currentColor = 0;
+	private float whenLevelIncreases;
+	private float blockStartPoint = 17f;
+	private float lastClonesXPos;
+	private bool allreadyCloned = false;
+	private int currentColor = 0;
+
+	private Vector3 thisClonePos;
+	private GameObject clone;
 
 	// Use this for initialization
 	void Start () {
-		blockCloner = FindObjectOfType<BlockCloner>();
 		whenLevelIncreases = increaseAmount;
 		PlayerPrefs.SetInt("Score", 0);
+		CloneBlock();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		IncreaseDiff ();
+
+		if (clone.transform.position.x <= blockStartPoint - BlockSep && !allreadyCloned) {
+			allreadyCloned = true;
+			CloneBlock();
+		}
+	}
+
+	void CloneBlock () {
+		thisClonePos = new Vector3(blockStartPoint, Random.Range(0.5f, 11.5f), 0f);
+		clone = Instantiate(block.gameObject, thisClonePos, Quaternion.identity) as GameObject;
+		allreadyCloned = false;
+	}
+
+	void IncreaseDiff () {
 		if (PlayerPrefs.GetInt("Score") >= whenLevelIncreases) {
-		
+
 			if (currentColor >= BgColor.Length) {
 				currentColor = 0;
 			}
-			
+
 			cam.backgroundColor = BgColor[currentColor];
 			currentColor += 1;
 			whenLevelIncreases += increaseAmount * 1.2f;
-			
+
 			if (Block.blockSpeed < 0.15f) {
 				Block.blockSpeed += BlockSpeedIncrease;
-				
-			} else if (blockCloner.BlockSep >= 4f) {
-				blockCloner.BlockSep -= blockSepDecrease;
+
+			} else if (BlockSep >= 4f) {
+				BlockSep -= blockSepDecrease;
 			}
 		}
 	}
